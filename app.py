4b02f2c261ac2322a812f7d7360f8621957e8477
metapropage <dianzia.adm@gmail.com>
2024-06-25 22:21:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 # Assuming the menu function is defined in a module named 'menu'
 from menu import menu
 
-    # Apply custom styling
-    st.markdown("""
-    <style>
-        #MainMenu, header, footer {visibility: hidden;}
-        section[data-testid="stSidebar"] div:first-child {top: 0; height: 100vh;}
-    </style>
-    """, unsafe_allow_html=True)
+# Apply custom styling
+st.markdown("""
+<style>
+    #MainMenu, header, footer {visibility: hidden;}
+    section[data-testid="stSidebar"] div:first-child {top: 0; height: 100vh;}
+</style>
+""", unsafe_allow_html=True)
 
 # Predefined username and password (for demonstration purposes)
 USERNAME = "admin"
