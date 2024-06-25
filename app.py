1eb64900690556fc1fc706b114700432c67c49b5
metapropage <dianzia.adm@gmail.com>
2024-06-25 18:44:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if st.session_state.authenticated:
     st.markdown("""
     ## Why Choose MetaPro?
 
-    **AI-Powered Precision:** Leverage the power of Google Generative AI to automatically generate highly relevant and descriptive titles and tags for your images. Enhance your image metadata with unprecedented accuracy and relevance.
+    **AI-Powered Precision:** Automatically generate highly relevant and descriptive titles and tags for your images. Enhance your image metadata with unprecedented accuracy and relevance.
 
     **Streamlined Workflow:** Upload your images in just a few clicks. Our app processes each photo, embeds the generated metadata, and prepares it for upload—automatically and effortlessly.
 
