1d5bd7482205bb1a06d572d04ed3a7d4998a7dc8
metapropage <dianzia.adm@gmail.com>
2024-06-26 04:34:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import os
 # Assuming the menu function is defined in a module named 'menu'
 from menu import menu
 
-# Main function for the Streamlit app
-
 # Apply custom styling
 st.markdown("""
-<style>
-    #MainMenu, header, footer {visibility: hidden;}
-    section[data-testid="stSidebar"] div:first-child {top: 0; height: 100vh;}
-</style>
-""", unsafe_allow_html=True)
+    <style>
+        #MainMenu, header, footer {
+            visibility: hidden;
+        }
+        section[data-testid="stSidebar"] {
+            top: 0;
+            height: 10vh;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 # Predefined username and password (for demonstration purposes)
 USERNAME = "admin"
