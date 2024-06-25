10ea34741f56c9daef0879e5dc906a3005f2a347
metapropage <dianzia.adm@gmail.com>
2024-06-25 21:43:53 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,12 +5,12 @@ def authenticated_menu():
     # Show a navigation menu for authenticated users
     st.sidebar.page_link("app.py", label="Home")
     st.sidebar.page_link("pages/user.py", label="Upload via Gdrive")
-    if st.session_state.role in ["admin", "super-admin"]:
+    if st.session_state.role in ["Prompts"]:
         st.sidebar.page_link("pages/admin.py", label="Upload via SFTP")
         st.sidebar.page_link(
-            "pages/super-admin.py",
+            "pages/Prompts.py",
             label="Describe Midjourney Prompts",
-            disabled=st.session_state.role != "super-admin",
+            disabled=st.session_state.role != "Prompts",
         )
 
 
