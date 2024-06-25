b18d149b4d1fd077a0d0d59b9cd121ea3f07f7b8
metapropage <dianzia.adm@gmail.com>
2024-06-25 23:01:53 +0700

    Update Prompts.py

--- pages/Prompts.py
+++ pages/Prompts.py
@@ -1,15 +1,6 @@
 import streamlit as st
-from menu import menu_with_redirect
 
-st.set_option("client.showSidebarNavigation", False)
-
-# Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
-
-# Verify the user's role
-if st.session_state.role not in ["super-admin"]:
-    st.warning("You do not have permission to view this page.")
-    st.stop()
-
-st.title("This page is available to super-admins")
-st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
+def display():
+    st.title("Admin Page")
+    st.write("Welcome to the Admin page.")
+    # Add more admin-specific content here
