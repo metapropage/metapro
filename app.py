c9544c254c483238bdbfce484a39d6f962d538b2
metapropage <dianzia.adm@gmail.com>
2024-06-25 22:54:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-from menu import authenticated_menu
+from menu import display_menu
 
-def menu_with_redirect():
-    # Redirect users to the main page if not logged in, otherwise continue to
-    # render the navigation menu
-    if "role" not in st.session_state or st.session_state.role is None:
-        st.experimental_rerun("app.py")
-    else:
-        authenticated_menu()
+def main():
+    st.title("Main App")
+    display_menu()
 
 if __name__ == "__main__":
-    menu_with_redirect()
+    main()
