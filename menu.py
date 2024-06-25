46fcff3f15722b9053a453948005b6bcb6ef66ac
metapropage <dianzia.adm@gmail.com>
2024-06-25 22:44:45 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,18 +1,16 @@
 import streamlit as st
 
-
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("app.py", label="Home")
-    st.sidebar.page_link("pages/Gdrive.py", label="Upload via Gdrive")
-    st.sidebar.page_link("pages/admin.py", label="Upload via SFTP")
-    st.sidebar.page_link("pages/Prompts.py", label="Upload via SFTP")
-        )
-
+    st.sidebar.page("Home", "app.py")
+    st.sidebar.page("Upload via Gdrive", "pages/Gdrive.py")
+    st.sidebar.page("Upload via SFTP (admin)", "pages/admin.py")
+    st.sidebar.page("Upload via SFTP (prompts)", "pages/Prompts.py")
 
 def menu_with_redirect():
     # Redirect users to the main page if not logged in, otherwise continue to
     # render the navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
-        st.switch_page("app.py")
-    menu()
+        st.experimental_rerun("app.py")
+    else:
+        authenticated_menu()
