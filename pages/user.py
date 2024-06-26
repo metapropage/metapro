3dce3b330157fe5947d03b3435be499091837ab1
metapropage <dianzia.adm@gmail.com>
2024-06-26 11:09:28 +0700

    Update user.py

--- pages/user.py
+++ pages/user.py
@@ -15,6 +15,7 @@ import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from menu import menu_with_redirect
 
 # Set the timezone to UTC+7 Jakarta
 JAKARTA_TZ = pytz.timezone('Asia/Jakarta')
