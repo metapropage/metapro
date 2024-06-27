4858416204e2c7d4e7ee87d4b2787ff404d6f2e5
metapropage <dianzia.adm@gmail.com>
2024-06-27 23:05:24 +0700

    Update super-admin.py

--- pages/super-admin.py
+++ pages/super-admin.py
@@ -58,17 +58,6 @@ def generate_prompts(image_paths):
 def main():
     """Main function for the Streamlit app."""
     
-    # Display WhatsApp chat link
-    st.markdown("""
-    <div style="text-align: center; margin-top: 20px;">
-        <a href="https://wa.me/6285328007533" target="_blank">
-            <button style="background-color: #1976d2; color: white; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer;">
-                MetaPro
-            </button>
-        </a>
-    </div>
-    """, unsafe_allow_html=True)
-
     # Check if license has already been validated
     license_file = "license.txt"
     if not st.session_state['license_validated']:
