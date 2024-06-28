aa66536662db9d5419973a47b731d943f85e42cd
metapropage <dianzia.adm@gmail.com>
2024-06-28 11:34:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if st.session_state.authenticated:
     
     **Subscribe Now and Experience the Difference:**
     
-    - Upgrade to the MetaPro Plan for just $* for a 2-month one-time payment! Enjoy these benefits:
+    -Upgrade to the MetaPro Plan for just $* for a 2-month one-time payment! Enjoy these benefits:
       
       **✓.** Private Website Link
 
