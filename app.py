c2f5760898c42d7a90c7653d73551e2f5e483045
metapropage <dianzia.adm@gmail.com>
2024-06-28 20:54:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Hide GitHub logo
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Predefined username and password (for demonstration purposes)
 USERNAME = "admin"
 PASSWORD = "dian"
