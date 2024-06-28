fcc34749cd39451d8c7a0692d50ee1aa9415a7de
metapropage <dianzia.adm@gmail.com>
2024-06-28 13:02:18 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
+
 def authenticated_menu():
     # Show a navigation menu for authenticated users
-    st.sidebar.page_link("app.py", label="Home")
-    st.sidebar.page_link("pages/user.py", label="Upload via Gdrive")
+    st.sidebar.page_link("app.py", label="Home", icon="🏠")
+    st.sidebar.page_link("pages/user.py", label="Upload via Gdrive", icon="📂")
     if st.session_state.role in ["admin", "super-admin"]:
-        st.sidebar.page_link("pages/admin.py", label="Upload via SFTP")
+        st.sidebar.page_link("pages/admin.py", label="Upload via SFTP", icon="🔑")
         st.sidebar.page_link(
             "pages/super-admin.py",
             label="Magic Prompts",
-            disabled=st.session_state.role != "super-admin"
+            disabled=st.session_state.role != "super-admin",
+            icon="✨"
         )
+
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
-    st.sidebar.page_link("app.py", label="Log in")
+    st.sidebar.page_link("app.py", label="Log in", icon="🔒")
+
 
 def menu():
     # Determine if a user is logged in or not, then show the correct
@@ -24,9 +28,11 @@ def menu():
         return
     authenticated_menu()
 
+
 def menu_with_redirect():
     # Redirect users to the main page if not logged in, otherwise continue to
     # render the navigation menu
     if "role" not in st.session_state or st.session_state.role is None:
         st.switch_page("app.py")
     menu()
+
