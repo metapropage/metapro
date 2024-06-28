d66ee333ae5a2e8fa0e00d26ca01cf3be975e084
metapropage <dianzia.adm@gmail.com>
2024-06-28 21:02:17 +0700

    Update user.py

--- pages/user.py
+++ pages/user.py
@@ -39,14 +39,15 @@ st.markdown("""
 st.markdown(
     """
     <style>
-    .css-1rs6os {
-        display: none !important;
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
-
 
 # Set the timezone to UTC+7 Jakarta
 JAKARTA_TZ = pytz.timezone('Asia/Jakarta')
