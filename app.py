9f0cefbf8290331cd80cadc5fbd504900821f603
metapropage <dianzia.adm@gmail.com>
2024-06-28 10:36:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,25 @@ if st.session_state.authenticated:
     # Additional Information
     st.markdown("### Why Choose MetaPro?")
     st.markdown("""
-    **AI-Powered Precision:** Leverage the power of Google Generative AI to automatically...
+        ### Why Choose MetaPro?
+
+    **AI-Powered Precision:** Leverage the power of Google Generative AI to automatically generate highly relevant and descriptive titles and tags for your images. Enhance your image metadata with unprecedented accuracy and relevance.
+
+    **Streamlined Workflow:** Upload your images in just a few clicks. Our app processes each photo, embeds the generated metadata, and prepares it for upload—automatically and effortlessly.
+
+    **Secure and Efficient Gdrive Upload:** Once processed, your images are securely uploaded to gdrive. Keep your workflow smooth and your data safe with our robust upload system.
+
+    *How It Works:
+    1. Upload Your Images: Drag and drop your JPG/JPEG files into the uploader.
+    2. Generate Metadata: Watch as the app uses AI to create descriptive titles and relevant tags.
+    3. Embed Metadata: The app embeds the metadata directly into your images.
+    4. Directly upload to Google Drive for faster downloads.
+    4. Directly upload to Google Drive for faster downloads.
+    
+    **Subscribe Now and Experience the Difference:**
+    - **MetaPro Standard Plan: $9.99/month – Upload up to 1000 images daily for 1 month, includes access to one Adobe Stock account.
+    - **MetaPro Premium Plan: $39.99/month – Unlimited image uploads and access to unlimited Adobe Stock accounts for lifetime.
+
+    Ready to revolutionize your workflow? Subscribe today and take the first step towards a smarter, more efficient image management solution.
+
     """)
