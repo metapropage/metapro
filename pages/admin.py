1a0e38d94c18f3a6d27e78c2c853372cabbd0bb6
metapropage <dianzia.adm@gmail.com>
2024-06-28 20:55:29 +0700

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -32,20 +32,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Hide GitHub logo
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Set the timezone to UTC+7 Jakarta
 JAKARTA_TZ = pytz.timezone('Asia/Jakarta')
 
