151cd5b893d5d5ac014daa1fef3f90cccffd6287
metapropage <dianzia.adm@gmail.com>
2024-06-29 14:44:13 +0700

    Update enhanced.py

--- pages/enhanced.py
+++ pages/enhanced.py
@@ -2,7 +2,7 @@ import streamlit as st
 from menu import menu_with_redirect
 
 # Set the page configuration first
-st.set_page_config(page_title="MetaPro MetaPro MetaPro MetaPro", page_icon="🪐")
+st.set_page_config(page_title="MetaPro", page_icon="🪐")
 
 # Disable sidebar navigation
 st.set_option("client.showSidebarNavigation", False)
