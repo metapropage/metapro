a2483802ee800375469c20caee140ac46339e32f
metapropage <dianzia.adm@gmail.com>
2024-06-29 13:05:39 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,16 +4,11 @@ import streamlit as st
 def authenticated_menu():
     # Show a navigation menu for authenticated users
     st.sidebar.page_link("app.py", label="Home", icon="🏠")
-    st.sidebar.page_link("pages/user.py", label="Upload via Gdrive", icon="🌐")
-    if st.session_state.role in ["admin", "super-admin"]:
-        st.sidebar.page_link("pages/admin.py", label="Upload via SFTP", icon="🚀")
-        st.sidebar.page_link(
-            "pages/super-admin.py",
-            label="Magic Prompts",
-            disabled=st.session_state.role != "super-admin",
-            icon="✨"
-        )
-
+    st.sidebar.page_link("pages/gdrive.py", label="Upload via Gdrive", icon="🌐")
+    st.sidebar.page_link("pages/sftp.py", label="Upload via SFTP", icon="🚀")
+    st.sidebar.page_link("pages/prompts.py", label="Magic Prompts", icon="✨")
+    st.sidebar.page_link("pages/enhanced.py", label="Enhanced Images", icon="🌐")
+    
 
 def unauthenticated_menu():
     # Show a navigation menu for unauthenticated users
