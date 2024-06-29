f7b68a2ee55f0f0fc031c1cfef18b791f5ce280e
metapropage <dianzia.adm@gmail.com>
2024-06-29 12:41:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
 
 # Predefined username and password (for demonstration purposes)
 USERNAME = "meta"
-PASSWORD = "bismillah"
+PASSWORD = "trial"
 
 # Initialize st.session_state variables
 if "authenticated" not in st.session_state:
