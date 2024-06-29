bfe086512fc2df372c6eee021a7b669fa407be10
metapropage <dianzia.adm@gmail.com>
2024-06-29 13:16:30 +0700

    Update enhanced.py

--- pages/enhanced.py
+++ pages/enhanced.py
@@ -1,33 +1,12 @@
 import streamlit as st
-from menu import menu_with_redirect
 
-# Set the page configuration first
-st.set_page_config(page_title="Coming Soon..", page_icon="🔜")
-
-# Disable sidebar navigation
-st.set_option("client.showSidebarNavigation", False)
-
-# Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
-
-# Apply custom styling
-st.markdown("""
-    <style>
-        #MainMenu, header, footer {
-            visibility: hidden;
-        }
-        section[data-testid="stSidebar"] {
-            top: 0;
-            height: 10vh;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+st.set_page_config(page_title="Coming Soon", page_icon="🔜")
 
 st.markdown(
     """
     <style>
     .main {
-        background: linear-gradient(135deg, #87CEEB 0%, #4682B4 100%);
+        background-color: #87CEEB; /* Sky blue color */
         color: #2e2e2e;
         display: flex;
         justify-content: center;
