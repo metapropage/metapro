24f34f2840416f2fdf7800b072152fc33e09439f
metapropage <dianzia.adm@gmail.com>
2024-07-01 13:42:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+
 import streamlit as st
-import os
 
 # Assuming the menu function is defined in a module named 'menu'
 from menu import menu
@@ -18,8 +18,8 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Predefined username and password (for demonstration purposes)
-USERNAME = "admin"
-PASSWORD = "dian"
+USERNAME = "meta"
+PASSWORD = "bismillah"
 
 # Initialize st.session_state variables
 if "authenticated" not in st.session_state:
@@ -36,36 +36,17 @@ def authenticate(username, password):
     if username == USERNAME and password == PASSWORD:
         st.session_state.authenticated = True
         st.session_state.role = "super-admin"  # Directly set the role to "super-admin"
-        set_lock("logged_in")
         st.session_state.rerun = True
     else:
         st.error("Incorrect username or password")
 
-# Function to check the lock file
-def check_lock():
-    lock_file = "lock.txt"
-    if os.path.exists(lock_file):
-        with open(lock_file, 'r') as file:
-            status = file.read()
-        return status == "logged_in"
-    return False
-
-# Function to set lock file
-def set_lock(status):
-    lock_file = "lock.txt"
-    with open(lock_file, 'w') as file:
-        file.write(status)
-
 # If the user is not authenticated, show the login form
 if not st.session_state.authenticated:
     st.title("Login")
-    if check_lock():
-        st.error("Another user is currently logged in. Please try again later.")
-    else:
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        if st.button("Login"):
-            authenticate(username, password)
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        authenticate(username, password)
 
 # If authenticated, show the menu and additional information
 if st.session_state.authenticated:
@@ -79,11 +60,48 @@ if st.session_state.authenticated:
     if st.sidebar.button("Logout"):
         st.session_state.authenticated = False
         st.session_state.role = None
-        set_lock("")
         st.success("Logged out successfully.")
 
     # Additional Information
     st.markdown("### Why Choose MetaPro?")
     st.markdown("""
-    **AI-Powered Precision:** Leverage the power of Google Generative AI to automatically...
+
+    **Website-based:** Easy to use and compatible with any operating system: Windows, macOS, Linux, and Android.
+    
+    **AI-Powered Precision:** Generate highly relevant and descriptive titles and tags for your images with high accuracy and relevance.
+    
+    **Streamlined Workflow:** Upload your images in just a few clicks. Our app processes each photo, embeds the generated metadata, and prepares it for upload automatically and effortlessly.
+
+    **Magic Prompts:** Generate Custom Prompts from Your Images and Create Similar Prompts with Ease! Download the Generated Prompts and Similar Prompts as Excel Files.
+
+    *How It Works:
+    1. Upload Your Images: Drag and drop your JPG/JPEG files into the uploader.
+    2. Generate Metadata: Watch as the app uses AI to create descriptive titles and relevant tags.
+    3. Embed Metadata: The app embeds the metadata directly into your images.
+    4. Securely Download with Embedded Metadata Directly to Your Private Google Drive Account.
+    4. Effortlessly and Quickly Send Images Embedded with Metadata via SFTP.
+    5. Magic Prompts: Generate Custom Prompts from Your Images and Create Similar Prompts with Ease
+    
+    **Subscribe Now and Experience the Difference:**
+    
+    - Upgrade to the MetaPro Basic Plan for just $0 for a 3-month.
+    
+    - Upgrade to the MetaPro Advanced Plan for just $0 for a lifetime.
+    
+    **Enjoy these benefits:**
+    
+      **✓.** Unlimited Upload
+      
+      **✓.** Private Website Link
+
+      **✓.** Private Google Drive Account
+      
+      **✓.** Unlock All Features
+      
+      **✓.** Free Daily Website Updates
+
+      **✓.** Customizable: Manually generate titles and tags with your own commands
+    
+    **Ready to revolutionize your workflow? Subscribe today and take the first step towards a smarter, more efficient image management solution.**
+
     """)
