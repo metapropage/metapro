b592fc45cad6e507e6fcd2efac0f87b0da96bcd7
metapropage <dianzia.adm@gmail.com>
2024-12-31 17:26:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import os
 from menu import menu
@@ -19,19 +20,23 @@ st.markdown("""
 USERNAME = "a"
 PASSWORD = "a"
 
-# Initialize session state variables
+# Initialize st.session_state variables
 if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 if "role" not in st.session_state:
     st.session_state.role = None
 
+if "rerun" not in st.session_state:
+    st.session_state.rerun = False
+
 # Authentication function
 def authenticate(username, password):
     if username == USERNAME and password == PASSWORD:
-        st.session_state.authenticated = True
+        st.session_state.authenticated = False
         st.session_state.role = "super-admin"  # Directly set the role to "super-admin"
         set_lock("logged_in")
+        st.session_state.rerun = False
     else:
         st.error("Incorrect username or password")
 
@@ -50,7 +55,7 @@ def set_lock(status):
     with open(lock_file, 'w') as file:
         file.write(status)
 
-# Main logic
+# If the user is not authenticated, show the login form
 if not st.session_state.authenticated:
     st.title("Login")
     if check_lock():
@@ -60,11 +65,13 @@ if not st.session_state.authenticated:
         password = st.text_input("Password", type="password")
         if st.button("Login"):
             authenticate(username, password)
-            if st.session_state.authenticated:
-                st.experimental_rerun()  # Rerun to redirect to the menu page
 
-# If authenticated, show the menu
+# If authenticated, show the menu and additional information
 if st.session_state.authenticated:
+    if st.session_state.rerun:
+        st.session_state.rerun = False
+        st.rerun()
+
     menu()  # Render the dynamic menu
 
     # Logout button in the sidebar
@@ -73,7 +80,6 @@ if st.session_state.authenticated:
         st.session_state.role = None
         set_lock("")
         st.success("Logged out successfully.")
-        st.experimental_rerun()
 
     # Additional Information
     st.markdown("### Why Choose MetaPro?")
